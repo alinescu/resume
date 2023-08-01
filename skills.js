f61ec9dca8cddc8f887ee0e2c2f395846e200205
Alin Florea <alinescu@gmail.com>
2023-08-01 01:21:35 -0400

    restructure

--- skills.js
+++ skills.js
@@ -1,17 +1,1 @@
-const skills = [
-
-['SQL', 11, 100],
-['Python', 7, 100],
-['Javascript', 7, 100],
-['CSS', 7, 70],
-['Neo4J', 7, 100],
-['PHP', 4, 100],
-['PL/pgSQL', 3, 90],
-['NIFI', 2, 80],
-['C/C++', 10, 80],
-['x86 Assembly', 10, 100],
-['ExtJS', 6, 100],
-['SQLAlchemy', 7, 100],
-['Unreal Engine', 1.5, 50]
-
-];
+skillsets/default.js
