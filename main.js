28a0089d4bcb60579e4a764b7d911617ccbc5fc6
Alin Florea <alinescu@gmail.com>
2023-08-01 01:07:47 -0400

    fix skillbar widths

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
 
 // populate skills
 
-let par = $('#skills');
+const skillbarWidth = 180;
+let parentDiv = $('#skills');
 
 for(let i = 0; i < skills.length; i++) {
 
     let skill = skills[i],
         name = skill[0],
         years = skill[1],
-        level = skill[2]
+        level = skill[2],
+        width = skillbarWidth * (level / 100);
 
-        let x = $('<div class="skill">' + name + '<div class="years">' + years + ' years</div><br/><div class="skill-bar _' + level + '"><div class="skill-bar-fill"></div></div></div>')
-        x.appendTo(par);
+        $('<div class="skill">' + name + '<div class="years">' + years + ' years</div><br/><div class="skill-bar"><div class="skill-bar-fill" style="width: ' + width + 'px"></div></div></div>').appendTo(parentDiv);
 }
 
 });
